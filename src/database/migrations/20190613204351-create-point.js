e9aafca44c9c70e0384d1416b939e8aa8528b60a
David Heyman <dave@axismaps.com>
2019-06-13 21:49:06 +0100

    Added points

---
+++ src/database/migrations/20190613204351-create-point.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Points', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: Sequelize.TEXT
+    },
+    firstyear: {
+      type: Sequelize.INTEGER
+    },
+    lastyear: {
+      type: Sequelize.INTEGER
+    },
+    firstdate: {
+      type: Sequelize.DATEONLY
+    },
+    lastdate: {
+      type: Sequelize.DATEONLY
+    },
+    TypeId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Types',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    geom: {
+      type: Sequelize.GEOMETRY('MULTIPOINT', 4326)
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Points')
+};
---
+++ src/models/point.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const Point = sequelize.define('Point', {
+    name: DataTypes.TEXT,
+    firstyear: DataTypes.INTEGER,
+    lastyear: DataTypes.INTEGER,
+    firstdate: DataTypes.DATEONLY,
+    lastdate: DataTypes.DATEONLY,
+    geom: DataTypes.GEOMETRY('MULTIPOINT', 4326)
+  }, {});
+  Point.associate = (models) => {
+    Point.belongsTo(models.Type);
+  };
+  return Point;
+};
