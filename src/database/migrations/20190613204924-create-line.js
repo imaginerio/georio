aad442e0eed8f60ebf253a63d257d66181a1305c
David Heyman <dave@axismaps.com>
2019-06-13 21:53:29 +0100

    Added lines

---
+++ src/database/migrations/20190613204924-create-line.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Lines', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: Sequelize.TEXT
+    },
+    firstyear: {
+      type: Sequelize.INTEGER
+    },
+    lastyear: {
+      type: Sequelize.INTEGER
+    },
+    firstdate: {
+      type: Sequelize.DATE
+    },
+    lastdate: {
+      type: Sequelize.DATE
+    },
+    TypeId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Types',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    geom: {
+      type: Sequelize.GEOMETRY('MULTILINESTRING', 4326)
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Lines')
+};
--- src/models/line.js
+++ src/models/line.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const Point = sequelize.define('Point', {
+  const Line = sequelize.define('Line', {
     name: DataTypes.TEXT,
     firstyear: DataTypes.INTEGER,
     lastyear: DataTypes.INTEGER,
     firstdate: DataTypes.DATEONLY,
     lastdate: DataTypes.DATEONLY,
-    geom: DataTypes.GEOMETRY('MULTIPOINT', 4326)
+    geom: DataTypes.GEOMETRY('MULTILINESTRING', 4326)
   }, {});
 
-  Point.associate = (models) => {
-    Point.belongsTo(models.Type);
+  Line.associate = (models) => {
+    Line.belongsTo(models.Type);
   };
 
-  return Point;
+  return Line;
 };
