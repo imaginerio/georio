14439d1d68720815de8a42b3ab7db6be846f4679
David Heyman <dave@axismaps.com>
2019-12-02 10:59:51 +0000

    Linted get-features

--- src/api/v1/get-features/get-features.controller.js
+++ src/api/v1/get-features/get-features.controller.js
@@ -1,6 +1,6 @@
 const httpStatus = require('http-status');
-const { Layer } = require('@models/');
 const _ = require('underscore');
+const { Layer } = require('@models/');
 
 /**
  * getFeatures
@@ -12,16 +12,16 @@ exports.getFeatures = async (req, res, next) => Layer.findByPk(req.params.layer)
       return layer.getGeo().then((features) => {
         const geojson = { type: 'FeatureCollection' };
         geojson.features = features.map((f) => {
-	  const properties = _.omit(f.dataValues, 'id', 'geom');
-	  const geometry = f.geom;
-	  const id = f.id;
-	  return {
-	    type: 'Feature',
-	    id,
-	    properties,
-	    geometry
-	  };
-	});
+          const properties = _.omit(f.dataValues, 'id', 'geom');
+          const geometry = f.geom;
+          const { id } = f;
+          return {
+            type: 'Feature',
+            id,
+            properties,
+            geometry
+          };
+        });
         res.status(httpStatus.OK);
         return res.json(geojson);
       });
