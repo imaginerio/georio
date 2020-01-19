0c078c553642f4ab5c11a1f8de1b3994578e7e1e
David Heyman <dave@axismaps.com>
2020-01-19 22:40:24 +0000

    Shapefile import service and bin script

---
+++ src/services/import-shapefile/import-shapefile.service.js
@@ -0,0 +1,62 @@
+/* eslint-disable consistent-return */
+const shp = require('shapefile');
+const newFeature = require('@services/new-feature');
+const { Type, Layer } = require('@models');
+
+const geoms = {
+  Polygon: 'polygon',
+  LineString: 'line',
+  Point: 'point'
+};
+
+/**
+ * ImportShapefile Service
+ *
+ */
+const importShapefileService = async (file, layer) => layer.getTypes()
+  .then((layerTypes) => {
+    const types = {};
+    layerTypes.forEach((t) => {
+      types[t.title] = t.id;
+    });
+    const model = layer.getGeomModel();
+    return model.destroy({
+      where: {},
+      include: [{
+        model: Type,
+        attributes: [],
+        required: true,
+        include: [{
+          model: Layer,
+          attributes: [],
+          where: {
+            id: layer.id
+          }
+        }]
+      }]
+    }).then(() => shp.open(file)
+      .then(source => source.read()
+        .then(async function log(result) {
+          if (result.done) return;
+          const { properties, geometry } = result.value;
+          let type;
+          if (Object.keys(types).includes(properties.type)) {
+            type = types[properties.type];
+          } else {
+            type = await Type.newType(layer.id, { title: properties.type });
+            types[properties.type] = type;
+          }
+          await newFeature({
+            type,
+            geometry: geoms[geometry.type],
+            dataType: 'geojson',
+            data: {
+              properties,
+              geometry
+            }
+          });
+          return source.read().then(log);
+        })));
+  });
+
+module.exports = importShapefileService;
