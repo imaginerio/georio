b1af7e2057d47b45d3f73c6571b47cd958040bc3
David Heyman <dave@axismaps.com>
2020-02-26 11:19:52 +0000

    Send viewcone JSON with visuals

--- src/api/v1/get-visual/get-visual.controller.js
+++ src/api/v1/get-visual/get-visual.controller.js
@@ -21,7 +21,8 @@ exports.getVisual = async (req, res, next) => {
       'credit',
       'creator',
       'VisualId',
-      [sequelize.fn('ST_Envelope', sequelize.col('geom')), 'extent'],
+      'geom',
+      [sequelize.fn('ST_Envelope', sequelize.col('geom')), 'box'],
       [sequelize.fn('ST_X', sequelize.col('point')), 'x'],
       [sequelize.fn('ST_Y', sequelize.col('point')), 'y']
     ],
@@ -42,14 +43,15 @@ exports.getVisual = async (req, res, next) => {
       if (docs.length) {
         visual.documents = docs.map((d) => {
           const doc = d;
-          doc.extent = [doc.extent.coordinates[0][0], doc.extent.coordinates[0][2]];
           doc.thumb = `https://s3.amazonaws.com/tilecache.axismaps.io/highways-waterways/thumbs/${doc.remoteid}.png`;
           if (!doc.x || !doc.y) {
+            doc.extent = [doc.box.coordinates[0][0], doc.box.coordinates[0][2]];
             doc.tiles = `https://s3.amazonaws.com/tilecache.axismaps.io/highways-waterways/${doc.remoteid}/{z}/{x}/{y}.png`;
           } else {
             doc.point = [doc.x, doc.y];
+            doc.viewcone = doc.geom;
           }
-          return _.omit(doc, 'VisualId', 'remoteid', 'x', 'y');
+          return _.omit(doc, 'VisualId', 'remoteid', 'geom', 'box', 'x', 'y');
         });
         response.push(visual);
       }
