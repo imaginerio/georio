cdb32aa70e187069e75129e08a4bf401adbef1ee
David Heyman <dave@axismaps.com>
2021-02-19 13:36:38 +0000

    Single origin

--- src/config/express.js
+++ src/config/express.js
@@ -40,7 +40,7 @@ app.use(helmet());
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors({
-  origin: process.env === 'production' ? /.*\.levantcarta\.org/ : 'http://localhost:3000',
+  origin: process.env === 'production' ? 'https://beta.beirut.levantcarta.org' : 'http://localhost:3000',
   credentials: true
 }));
 
