41819ee9f64243f3b76d7a6468ea8c9b9c2f796c
David Heyman <dave@axismaps.com>
2021-07-20 15:55:07 +0100

    Fixes changeset problem

--- src/services/create-changeset/create-changeset.service.js
+++ src/services/create-changeset/create-changeset.service.js
@@ -1,18 +1,19 @@
 const { flatten } = require('underscore');
-const { Changeset } = require('@models/');
+const { User, Changeset } = require('@models/');
 
 /**
  * CreateChangeset Service
  *
  */
-const createChangesetService = async (user, title) => Promise.all([user.getPolygons(), user.getLines(), user.getPoints()])
-  .then(async (changesRaw) => {
-    const changes = flatten(changesRaw);
-    if (changes.length) {
-      const changeset = await Changeset.create({ title, userId: user.id });
-      return Promise.all(changes.map(change => change.setChangeset(changeset))).then(() => changeset);
-    }
-    return Promise.reject(new Error('No active changes'));
-  });
+const createChangesetService = async (userRaw, title) => User.findByPk(userRaw.id)
+  .then(user => Promise.all([user.getPolygons(), user.getLines(), user.getPoints()])
+    .then(async (changesRaw) => {
+      const changes = flatten(changesRaw);
+      if (changes.length) {
+        const changeset = await Changeset.create({ title, UserId: user.id });
+        return Promise.all(changes.map(change => change.setChangeset(changeset))).then(() => changeset);
+      }
+      return Promise.reject(new Error('No active changes'));
+    }));
 
 module.exports = createChangesetService;
